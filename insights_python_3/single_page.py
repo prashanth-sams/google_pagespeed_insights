35b83f2cdea287f440f41b719063ce1eea5ffd46
prashanth-sams <sams.prashanth@gmail.com>
2019-05-21 16:46:18 +0400

    update doc

--- insights_python_3/single_page.py
+++ insights_python_3/single_page.py
@@ -2,6 +2,7 @@ import requests, json, sys
 
 with open("config.json", "r") as read_file:
     
+    # read config.json file
     data = json.loads(read_file.read())
     
     strategy = sys.argv[1]
@@ -11,6 +12,7 @@ with open("config.json", "r") as read_file:
     
     print('strategy : ' + strategy + '\npage : ' + page + '\nurl : ' + url + '\n')
 
+    # start writing data in csv file
     file = open('results.csv', 'w')
     column_title = "URL, First Meaningful Paint, First Contentful Paint, Speed Index, Interactive\n"
     file.write(column_title)
@@ -18,14 +20,17 @@ with open("config.json", "r") as read_file:
     googleinsight_api = f'{api_base_url}?url={url}&strategy={sys.argv[1]}'
     print(f'Sending API request: {googleinsight_api}')
 
+    # process api request
     r = requests.get(googleinsight_api)
     final_dump = r.json()
     
+    # manipulate data
     first_contentful_paint = str(final_dump['lighthouseResult']['audits']['first-contentful-paint']['displayValue'])
     first_meaningful_paint = str(final_dump['lighthouseResult']['audits']['first-meaningful-paint']['displayValue'])
     speed_index = str(final_dump['lighthouseResult']['audits']['speed-index']['displayValue'])
     interactive = str(final_dump['lighthouseResult']['audits']['interactive']['displayValue'])
     
+    # print in console
     print(f'first contentful paint: ' + first_contentful_paint)
     print(f'first meaningful paint: ' + first_meaningful_paint)
     print(f'speed index: ' + speed_index)
